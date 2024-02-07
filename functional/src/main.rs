7116452287ae13eaf2bfb7c9d3dee0f3e0d530e4
Hyeong Jun Noh <munhwas1140@naver.com>
2024-02-07 22:31:23 +0900

    2024-02-07

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -61,4 +61,10 @@ fn main() {
         .unwrap();
 
     let id = thread::current().id();
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_>= v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+
 }
